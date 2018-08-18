9f7e01d25cd8482f1e0efed65dacd67538ae0f97
boboidream <z644381492@gmail.com>
2018-08-19 02:11:16 +0800

    [*] version 0.0.1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var fs = require('hexo-fs');
 var del = require('del') 
 var path = require('path')
 var request = require('request');
+var Eventproxy = require('eventproxy');
+const copyFileSync = require('fs-copy-file-sync');
 const readChunk = require('read-chunk'); 
 const imageType = require('image-type');
 const sharp = require('sharp');
@@ -12,10 +14,11 @@ var log = require('hexo-log')({
   silent: false
 });
 
-const config = this.config.download_images
-
 
 hexo.extend.processor.register('posts/:id.md', function(file) {
+  const config = this.config.easy_images
+  var ep = new Eventproxy()
+
   if (file.type == 'delete') return file
   if (file.type == 'skip' && config && !config.init) return file
 
@@ -44,6 +47,8 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
   var dir_post = path.dirname(file.source)
   var dir_source = this.source_dir
   var dir_images = path.join(dir_source, 'images', file.params.id)
+
+  if (!fs.existsSync(dir_images)) fs.mkdirsSync(dir_images)
   
   absolute_images.forEach(img => {
     let info = {
@@ -100,31 +105,68 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
     }
   }
 
+  // 处理图片与 Post
+  ep.after('download', online_images.length, (list) => {
+    var mapObj = {}
+
+    diff_list.forEach(info => {
+      //Copy 本地图片
+      if (info.from && info.from != info.to) {
+        copyFileSync(info.from, info.to)
+        info.to = fixExt(info.to)
+      }
+
+      //压缩图片
+      if (config.max_width || config.max_height){
+        info.to = resizeImg(info.to, config.max_width, config.max_height)
+        info.new = path.relative(dir_post, info.to)
+      }
+
+      //替换图片新地址
+      if (info.new && info.origin != info.new) {
+        info.origin = info.origin.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")
+        mapObj[info.origin] = info.new 
+      }
+    })
+
+
+    if (Object.keys(mapObj).length) content = replaceAll(content, mapObj) 
+
+    fs.writeFile(file.source, content)
+    return file
+
+  })
+
   function fixExt(path_img) {
     let buffer = readChunk.sync(path_img, 0, 12);
     let origin_ext = path.extname(path_img) 
     let real = imageType(buffer);
+    let real_ext = '.' + real.ext
     let path_res = path_img
 
-    if (origin_ext != real.ext) {
-      path_res = path_img.replace(origin_ext, real.ext)
-
-      if (fs.renameSync(path_img, path_res)) {
-        log.info(`Fix ${path_img}` to `${real.ext} success!`)
-      } else {
+    if (origin_ext.toLowerCase() != real_ext) {
+      path_res = path_img.replace(origin_ext, real_ext)
+      
+      try {
+        fs.renameSync(path_img, path_res) 
+      } catch(err) {
         log.error(`Rename ${path_img} error.`)
       }      
+
+      log.info(`Fix ${path_img} to ${real.ext} success!`)
     }
 
     return path_res
   }
 
   function resizeImg(path_img, w, h) {
+    if (!path_img) log.warn(`path_img is empty.`)
     if (path.extname(path_img).toLowerCase() == '.gif') return path_img
     
     let dimensions = sizeOf(path_img)
+    let path_res = path_img
 
-    if (dimensions.width > w or dimensions.height > h) {
+    if (dimensions.width > w || dimensions.height > h) {
       let path_res = path_img
       let img_ext = path.extname(path_img)
       
@@ -134,6 +176,7 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
         .max()
         .withoutEnlargement()
         .toFile(path_res)
+        .then(data => log.info(`Resize ${path_img} success.`))
         .catch(e => log.error(e))
     }
 
@@ -145,7 +188,11 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
     let dest = path.join(dir_images, name)
     let new_path = ''
     
-    if (request(src).pipe(fs.ensureWriteStreamSync(dest))) {
+    let ws = fs.ensureWriteStreamSync(dest)
+
+    request(src).pipe(ws)
+      
+    ws.on('finish', () => {
       dest = fixExt(dest)
       new_path = path.relative(dir_post, dest)
 
@@ -153,56 +200,33 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
         origin: src,
         new: new_path,
         from: false,
-        to: false
+        to: dest 
       }) 
-    } else {
+
+      ep.emit('download', new_path)
+
+    }).on('error', (err) => {
       log.error(`Download ${src} failed.`)
-    }
+    })
 
   }
   
-  var mapObj = {}
-
-  diff_list.forEach(info => {
-    //Copy 本地图片
-    if (info.to && info.from != info.to) {
-      fs.copyFile(info.from, info.to)
-      info.to = fixExt(info.to)
-    }
-
-    //压缩图片
-    if (config.max_width || config.max_height){
-      info.to = resizeImg(info.to, config.max_width, config.max_height)
-    }
-
-    //替换图片新地址
-    if (info.new && info.origin != info.new) {
-      info.origin = info.origin.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")
-      mapObj[info.origin] = info.new 
-    }
-  })
 
   function replaceAll(str, mapObj){
     var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 
     return str.replace(re, function(matched){
-      console.log(matched)
       return mapObj[matched.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")];
     });
   }
 
+
   
-  if (Object.keys(mapObj).length) content = replaceAll(content, mapObj) 
-
-  fs.writeFile(file.source, content)
-
-  file.type = 'update'
-
-  return file
 })
 
 //将相对地址转为绝对地址
 hexo.extend.filter.register('before_post_render', function(data){
+  const config = this.config.easy_images
   var dir_post = path.join(this.source_dir, data.source)
   var pattern = /!\[(.*?)\]\((.*?)\)/g
 
@@ -212,12 +236,11 @@ hexo.extend.filter.register('before_post_render', function(data){
     src_new = path_img.replace(this.source_dir, '/')
     
     if (config && config.cdn_prefix) {
-      src_new = path.join(cdn_prefix, src_new)
+       src_new = config.cdn_prefix + src_new
     }
 
     return `![${alt}](${src_new})`
   })
 
-  console.log(data.content)
   return data;
 });
