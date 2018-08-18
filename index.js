e22ec09eab5e9d13ffa791fcb1ca460b2cd03694
boboidream <z644381492@gmail.com>
2018-08-18 23:08:11 +0800

    [+] fixExt, resizeImg

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ var fs = require('hexo-fs');
 var del = require('del') 
 var path = require('path')
 var request = require('request');
+const readChunk = require('read-chunk'); 
+const imageType = require('image-type');
+const sharp = require('sharp');
+const sizeOf = require('image-size');
 var log = require('hexo-log')({
   debug: false,
   silent: false
 });
 
-hexo.extend.processor.register('posts/:id.md', function(file) {
-  var config = this.config.download_images
+const config = this.config.download_images
 
-  console.log(file)
+
+hexo.extend.processor.register('posts/:id.md', function(file) {
   if (file.type == 'delete') return file
   if (file.type == 'skip' && config && !config.init) return file
 
@@ -40,8 +44,8 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
   var dir_post = path.dirname(file.source)
   var dir_source = this.source_dir
   var dir_images = path.join(dir_source, 'images', file.params.id)
-  //console.log(this)
-  absolute_images.map(img => {
+  
+  absolute_images.forEach(img => {
     let info = {
       origin: img
     }
@@ -96,12 +100,55 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
     }
   }
 
+  function fixExt(path_img) {
+    let buffer = readChunk.sync(path_img, 0, 12);
+    let origin_ext = path.extname(path_img) 
+    let real = imageType(buffer);
+    let path_res = path_img
+
+    if (origin_ext != real.ext) {
+      path_res = path_img.replace(origin_ext, real.ext)
+
+      if (fs.renameSync(path_img, path_res)) {
+        log.info(`Fix ${path_img}` to `${real.ext} success!`)
+      } else {
+        log.error(`Rename ${path_img} error.`)
+      }      
+    }
+
+    return path_res
+  }
+
+  function resizeImg(path_img, w, h) {
+    if (path.extname(path_img).toLowerCase() == '.gif') return path_img
+    
+    let dimensions = sizeOf(path_img)
+
+    if (dimensions.width > w or dimensions.height > h) {
+      let path_res = path_img
+      let img_ext = path.extname(path_img)
+      
+      path_res = path_img.replace(img_ext, '-s' + img_ext) 
+      sharp(path_img)
+        .resize(w, h)
+        .max()
+        .withoutEnlargement()
+        .toFile(path_res)
+        .catch(e => log.error(e))
+    }
+
+    return path_res
+  }
+
   function downloadImg(src) {
     let name = 'ol-' + Date.now() + '.png'
     let dest = path.join(dir_images, name)
-    let new_path = path.relative(dir_post, dest)
+    let new_path = ''
     
     if (request(src).pipe(fs.ensureWriteStreamSync(dest))) {
+      dest = fixExt(dest)
+      new_path = path.relative(dir_post, dest)
+
       diff_list.push({
         origin: src,
         new: new_path,
@@ -117,11 +164,22 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
   var mapObj = {}
 
   diff_list.forEach(info => {
+    //Copy 本地图片
     if (info.to && info.from != info.to) {
       fs.copyFile(info.from, info.to)
+      info.to = fixExt(info.to)
     }
-    info.origin = info.origin.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")
-    mapObj[info.origin] = info.new 
+
+    //压缩图片
+    if (config.max_width || config.max_height){
+      info.to = resizeImg(info.to, config.max_width, config.max_height)
+    }
+
+    //替换图片新地址
+    if (info.new && info.origin != info.new) {
+      info.origin = info.origin.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")
+      mapObj[info.origin] = info.new 
+    }
   })
 
   function replaceAll(str, mapObj){
@@ -133,8 +191,33 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
     });
   }
 
-  content = replaceAll(content, mapObj) 
+  
+  if (Object.keys(mapObj).length) content = replaceAll(content, mapObj) 
+
   fs.writeFile(file.source, content)
+
+  file.type = 'update'
 
   return file
 })
+
+//将相对地址转为绝对地址
+hexo.extend.filter.register('before_post_render', function(data){
+  var dir_post = path.join(this.source_dir, data.source)
+  var pattern = /!\[(.*?)\]\((.*?)\)/g
+
+  data.content = data.content.replace(pattern, (match, alt, src) => {
+    let path_img = path.resolve(dir_post, '..', src)
+
+    src_new = path_img.replace(this.source_dir, '/')
+    
+    if (config && config.cdn_prefix) {
+      src_new = path.join(cdn_prefix, src_new)
+    }
+
+    return `![${alt}](${src_new})`
+  })
+
+  console.log(data.content)
+  return data;
+});
