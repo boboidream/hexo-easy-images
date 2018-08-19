bb0cff7c3d17e2777f4065b40490686b46cf8bad
boboidream <z644381492@gmail.com>
2018-08-19 15:17:45 +0800

    [+] readme

---
+++ README.md
@@ -0,0 +1,100 @@
+## hexo-easy-images
+
+### 安装
+```bash
+npm i hexo-easy-images -s
+```
+
+### 自定义配置
+
+以下配置非必需，在 `your-hexo-project/_config.yml` 添加。
+
+```yml
+# Easy images
+easy_images:
+  init: false # 默认 false；改为 true，将处理过去所有 post，通常装插件后第一次运行时使用。
+  cdn_prefix: http://yourcdn.com # 默认 null; 图片前缀，在生成 html 文件时为 image 添加前缀
+  max_width: 800 # 默认 null；设置图片最大宽度
+  max_height: 800 # 默认 null；设置图片最大高度
+```
+
+### 介绍
+在书写 post 时，再也不用为插入图片问题担心了。只要你定义图片路径正确，无论是本地、在线、绝对路径、相对路径，统统不用担心。
+
+本插件自动将 post 中使用到图片，copy 到 source/images/your_post_id 文件夹下，并将 post文件内图片引用路径改为本地相对路径，保证本地 Markdown 编辑器正确显示。
+
+同时在生成 html 文件时，会将 html 中图片路径改为绝对路径。故，无论本地线上，图片都无需担心。
+
+例如：post 内容如下
+
+```md
+---
+title: Hello World
+---
+
+## 本地图片（绝对路径）
+![test](/Users/zhangwenbo/Coding/hexo/public/img/2014-03-11.jpg)
+![](~/Coding/hexo/public/img/2014-03-30-2.jpg)
+
+## 本地图片（相对路径）
+![](./2014-03-30.jpg)
+![](../images/000.jpg)
+
+## 在线图片
+![小猫](https://images.unsplash.com/photo-1534201569625-ed4662d8be97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1ff48aebbb7e08c289f8f738b5592f47&auto=format&fit=crop&w=400&q=60)
+
+## CDN 图片
+![艺](http://7xqmgi.com1.z0.glb.clouddn.com/img/2014-03-11.jpg)
+
+无论哪种方式都能完美显示！
+```
+
+运行 `hexo g` 后，所有图片被 copy 或 download 到 `source/images/hello-world-2` 下，且 post 内容变为
+
+```md
+---
+title: Hello World
+---
+
+## 本地图片（绝对路径）
+![test](../images/hello-world-2/2014-03-11.jpg)
+![](../images/hello-world-2/2014-03-30-2.jpg)
+
+## 本地图片（相对路径）
+![](../images/hello-world-2/2014-03-30.jpg)
+![](../images/hello-world-2/000.jpg)
+
+## 在线图片
+![小猫](../images/hello-world-2/ol-1534652801825.jpg)
+
+## CDN 图片
+![艺](../images/hello-world-2/ol-1534652801828.jpg)
+
+无论哪种方式都能完美显示！
+```
+
+生成的 html 文件为：
+```html
+<h2 id="本地图片（绝对路径）"><a href="#本地图片（绝对路径）" class="headerlink" title="本地图片（绝对路径）"></a>本地图片（绝对路径）</h2>
+<p><img src="/images/hello-world-2/2014-03-11-s.jpg" alt="test"><br><img src="/images/hello-world-2/2014-03-30-2-s.jpg" alt=""></p>
+<h2 id="本地图片（相对路径）"><a href="#本地图片（相对路径）" class="headerlink" title="本地图片（相对路径）"></a>本地图片（相对路径）</h2>
+<p><img src="/images/hello-world-2/2014-03-30-s.jpg" alt=""><br><img src="/images/hello-world-2/000-s.jpg" alt=""></p>
+<h2 id="在线图片"><a href="#在线图片" class="headerlink" title="在线图片"></a>在线图片</h2>
+<p><img src="/images/hello-world-2/ol-1534652801825-s.jpg" alt="小猫"></p>
+<h2 id="CDN-图片"><a href="#CDN-图片" class="headerlink" title="CDN 图片"></a>CDN 图片</h2>
+<p><img src="/images/hello-world-2/ol-1534652801828-s.jpg" alt="艺"></p>
+<p>无论哪种方式都能完美显示！</p>
+```
+
+如此，在本地还是线上地图片，都完美显示。
+
+另可自定义生成压缩版（后缀为 `imgname-s.jpg`）图片。也可配置 `cnd_prefix` 则生成 html 图片路径将为：
+```html
+<h2 id="CDN-图片"><a href="#CDN-图片" class="headerlink" title="CDN 图片"></a>CDN 图片</h2>
+<p><img src="http://yourcdn.com/images/hello-world-2/ol-1534652801828-s.jpg" alt="艺"></p>
+```
+
+## TODO
+[-] 发布到 hexo 插件
+[-] 在 windows 上测试
+
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var os = require('os')
-var fs = require('hexo-fs');
-var del = require('del') 
-var path = require('path')
-var request = require('request');
-var Eventproxy = require('eventproxy');
+const os = require('os')
+const fs = require('hexo-fs');
+const del = require('del')
+const path = require('path')
+const request = require('request');
+const Eventproxy = require('eventproxy');
 const copyFileSync = require('fs-copy-file-sync');
-const readChunk = require('read-chunk'); 
+const readChunk = require('read-chunk');
 const imageType = require('image-type');
 const sharp = require('sharp');
 const sizeOf = require('image-size');
-var log = require('hexo-log')({
+const log = require('hexo-log')({
   debug: false,
   silent: false
 });
 
-
+// 处理 post 中图片
 hexo.extend.processor.register('posts/:id.md', function(file) {
   const config = this.config.easy_images
-  var ep = new Eventproxy()
+  const ep = new Eventproxy()
 
   if (file.type == 'delete') return file
   if (file.type == 'skip' && config && !config.init) return file
 
-  //获取图片地址
+  //获取图片列表
   var content =  fs.readFileSync(file.source)
-  var pattern = /!\[.*?\]\((.*?)\)/g 
+  var pattern = /!\[.*?\]\((.*?)\)/g
   var absolute_images = []
   var relative_images = []
   var online_images = []
@@ -32,6 +32,7 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
 
   while((match=pattern.exec(content)) != null) {
     let url = match[1]
+
     if (url[0] == '/' || url[0] == '~') {
       absolute_images.push(url)
     } else if (/^http/.test(url)) {
@@ -40,7 +41,7 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
       relative_images.push(url)
     }
   }
-  
+
   if (absolute_images.length + relative_images.length + online_images.length  == 0) return file
 
   var dir_root = this.base_dir
@@ -49,29 +50,34 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
   var dir_images = path.join(dir_source, 'images', file.params.id)
 
   if (!fs.existsSync(dir_images)) fs.mkdirsSync(dir_images)
-  
+
+  // 将绝对路径图片加入待处理队列 diff_list
   absolute_images.forEach(img => {
     let info = {
       origin: img
     }
 
-    if (img[0] == '~') img = os.homedir() + img.substring(1) 
+    if (img[0] == '~') img = os.homedir() + img.substring(1)
 
-    if (fs.existsSync(img)) info.from = img 
-    
-    if (fs.existsSync(path.join(dir_root, img))) info.from = path.join(dir_root, img)
-
-    if (!info.from) {
+    if (fs.existsSync(img)) {
+      info.from = img
+    } else if (fs.existsSync(path.join(dir_root, img))) {
+      info.from = path.join(dir_root, img)
+    } else {
       log.warn(`${file.id}: Can't find ${img}`)
       return
     }
 
+    if (path.dirname(info.from) == dir_images) {
+      info.skip_copy = true
+    }
+
     info.to = path.join(dir_images, path.basename(info.from))
-    log.info(dir_post+','+info.to)
     info.new = path.relative(dir_post, info.to)
     diff_list.push(info)
   })
-  
+
+  // 将相对路径图片加入待处理队列 diff_list
   relative_images.forEach(img => {
     let info = {
       origin: img
@@ -82,16 +88,18 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
     if (!fs.existsSync(info.from)) {
       log.warn(`${file.id}: Can't find ${img}`)
       return
-    } 
+    }
 
-    if (path.dirname(info.from) == dir_images) return
+    if (path.dirname(info.from) == dir_images) {
+      info.skip_copy = true
+    }
 
     info.to = path.join(dir_images, path.basename(info.from))
     info.new = path.relative(dir_post, info.to)
-    diff_list.push(info) 
+    diff_list.push(info)
   })
 
-  // 下载图片 
+  // 下载图片
   if (online_images.length) {
     var Bagpipe = require('bagpipe')
     var bagpipe = new Bagpipe(10)
@@ -99,7 +107,7 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
     del.sync(dir_images + '/ol-*')
 
     for(var i = 0; i < online_images.length; i++) {
-      bagpipe.push(downloadImg, online_images[i], function(e, data){
+      bagpipe.push(downloadImg, online_images[i], function(e){
         log.error(e)
       })
     }
@@ -111,10 +119,12 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
 
     diff_list.forEach(info => {
       //Copy 本地图片
-      if (info.from && info.from != info.to) {
+      if (!info.skip_copy) {
         copyFileSync(info.from, info.to)
-        info.to = fixExt(info.to)
       }
+
+      //修正图片后缀
+      info.to = fixExt(info.to)
 
       //压缩图片
       if (config.max_width || config.max_height){
@@ -125,34 +135,33 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
       //替换图片新地址
       if (info.new && info.origin != info.new) {
         info.origin = info.origin.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")
-        mapObj[info.origin] = info.new 
+        mapObj[info.origin] = info.new
       }
     })
 
 
-    if (Object.keys(mapObj).length) content = replaceAll(content, mapObj) 
+    if (Object.keys(mapObj).length) content = replaceAll(content, mapObj)
 
     fs.writeFile(file.source, content)
     return file
-
   })
 
+  ep.fail((err) => {
+    log.error(err)
+  })
+
+
+  //-------------------------------------------------------------
   function fixExt(path_img) {
     let buffer = readChunk.sync(path_img, 0, 12);
-    let origin_ext = path.extname(path_img) 
+    let origin_ext = path.extname(path_img)
     let real = imageType(buffer);
     let real_ext = '.' + real.ext
     let path_res = path_img
 
     if (origin_ext.toLowerCase() != real_ext) {
       path_res = path_img.replace(origin_ext, real_ext)
-      
-      try {
-        fs.renameSync(path_img, path_res) 
-      } catch(err) {
-        log.error(`Rename ${path_img} error.`)
-      }      
-
+      fs.renameSync(path_img, path_res)
       log.info(`Fix ${path_img} to ${real.ext} success!`)
     }
 
@@ -162,15 +171,16 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
   function resizeImg(path_img, w, h) {
     if (!path_img) log.warn(`path_img is empty.`)
     if (path.extname(path_img).toLowerCase() == '.gif') return path_img
-    
+
     let dimensions = sizeOf(path_img)
-    let path_res = path_img
+    var path_res = path_img
 
     if (dimensions.width > w || dimensions.height > h) {
       let path_res = path_img
       let img_ext = path.extname(path_img)
-      
-      path_res = path_img.replace(img_ext, '-s' + img_ext) 
+
+      path_res = path_img.replace(img_ext, '-s' + img_ext)
+
       sharp(path_img)
         .resize(w, h)
         .max()
@@ -178,64 +188,70 @@ hexo.extend.processor.register('posts/:id.md', function(file) {
         .toFile(path_res)
         .then(data => log.info(`Resize ${path_img} success.`))
         .catch(e => log.error(e))
-    }
 
-    return path_res
+      return path_res
+    } else {
+      return path_res
+    }
   }
 
   function downloadImg(src) {
-    let name = 'ol-' + Date.now() + '.png'
+    let name = 'ol-' + Date.now() + '.jpg'
     let dest = path.join(dir_images, name)
-    let new_path = ''
-    
     let ws = fs.ensureWriteStreamSync(dest)
+    let new_path = ''
 
-    request(src).pipe(ws)
-      
+    request(src).on('error', (err) => {
+      del.sync(dest)
+      log.warn(`Download ${src} failed.`)
+    }).pipe(ws)
+
     ws.on('finish', () => {
-      dest = fixExt(dest)
       new_path = path.relative(dir_post, dest)
 
       diff_list.push({
         origin: src,
         new: new_path,
         from: false,
-        to: dest 
-      }) 
+        to: dest,
+        skip_copy: true
+      })
 
       ep.emit('download', new_path)
-
     }).on('error', (err) => {
-      log.error(`Download ${src} failed.`)
+      log.error(`Write ${dest} failed.`)
+      log.error(err)
+      del.sync(dest)
     })
-
   }
-  
+
 
   function replaceAll(str, mapObj){
     var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 
     return str.replace(re, function(matched){
-      return mapObj[matched.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")];
+      return mapObj[matched.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")]
     });
   }
 
-
-  
 })
 
 //将相对地址转为绝对地址
 hexo.extend.filter.register('before_post_render', function(data){
   const config = this.config.easy_images
   var dir_post = path.join(this.source_dir, data.source)
+  var dir_images = path.join(this.source_dir, 'images', data.slug)
   var pattern = /!\[(.*?)\]\((.*?)\)/g
 
   data.content = data.content.replace(pattern, (match, alt, src) => {
-    let path_img = path.resolve(dir_post, '..', src)
+    if (path.dirname(src) != path.relative(path.dirname(dir_post), dir_images)) {
+      return match
+    }
 
-    src_new = path_img.replace(this.source_dir, '/')
-    
-    if (config && config.cdn_prefix) {
+    let path_img = path.resolve(dir_post, '..', src)
+    let src_new = path_img.replace(this.source_dir, '/')
+
+    if (config && config.cdn_prefix && src_new[0] == '/') {
        src_new = config.cdn_prefix + src_new
     }
 
